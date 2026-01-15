5294db396e4874e84d4c3cca1504f82e90b9aaba
Danny Labes <daniel.andrew.labes@gmail.com>
2026-01-15 16:57:04 -0500

    Lessons 8-13

---
+++ courses/python-for-beginners/lessons_12.py
@@ -0,0 +1,11 @@
+integer_type = 6
+float_type = 7.0
+bool_type = True
+string_type = "Test String"
+list_type = [1,2,3]
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
---
+++ courses/python-for-beginners/lessons_13.py
@@ -0,0 +1,10 @@
+variable = 6
+print(type(variable))
+variable = 7.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "String"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
